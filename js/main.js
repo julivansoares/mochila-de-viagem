8061123b31647f9ac0cd897ecf6e3c4c953c0621
julivansoares <julivan.soares@outlook.com>
2023-06-07 19:38:31 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -16,16 +16,31 @@ form.addEventListener("submit", (evento) => {
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
 
+  const existe = itens.find(elemento=>elemento.nome === nome.value)
+
+  console.log(existe);
+
   const itemAtual = {
     // objeto armazena os dados que quero para o local storage pq vai ser mais de um dados
     nome: nome.value,
     quantidade: quantidade.value,
   };
 
-  criaElemento(itemAtual); //aqui chamo a função cria elementos já passando os dados no parametro
+  if (existe) {
+    itemAtual.id = existe.id;
 
-  itens.push(itemAtual); // agora coloco meu objeto dentro do arrey item pq se eu colocar só o objeto meu local storage vai sobreescrever e com o array não
+    atualizaElemento(itemAtual);
 
+    itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    
+  } else {
+    itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id+1 :0;
+
+    criaElemento(itemAtual); //aqui chamo a função cria elementos já passando os dados no parametro
+
+    itens.push(itemAtual); // agora coloco meu objeto dentro do arrey item pq se eu colocar só o objeto meu local storage vai sobreescrever e com o array não
+  }
+  
   localStorage.setItem("itens", JSON.stringify(itens)); // salvando meu array de objetos dentro do meu local storage o local storage só aceita string então transformo meu array de objetos em string
   //localStorage.setItem("nome",nome); // aqui estou salvando um dadono meu localStorade ex("chave_identifica o dado",dado)
 
@@ -44,10 +59,40 @@ function criaElemento(item) {
 
   numeroItem.innerHTML = item.quantidade; // nessa parte o innerHTML serve para colocar um dado dentro da minha teg html
 
+  numeroItem.dataset.id= item.id
+
   novoItem.appendChild(numeroItem); // para colocar uma teg dentro da outra utilizo  appendChild
 
   
   novoItem.innerHTML += item.nome; // aqui coloco o nome dentro da teg strong
 
+  novoItem.appendChild(botaoDelete(item.id))
+
   lista.appendChild(novoItem); // agora pego minha li que já contem o strong e jogo dentro da minha lista
+}
+
+
+function atualizaElemento (item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+    
+}
+
+
+function botaoDelete(id) {
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerHTML = "x";
+
+  elementoBotao.addEventListener("click", function() {
+     deletaElemento(this.parentNode, id);
+  });
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag,id) {
+  tag.remove();
+
+  itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+  localStorage.setItem("itens", JSON.stringify(itens));
+  console.log(itens);
 }
